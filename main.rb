24b71ee6e1f8cb6c025688518a28addab066453b
vagrant <vagrant@vagrant.vm>
2019-04-03 17:21:50 +0000

    New method to detect duplicate cities

--- main.rb
+++ main.rb
@@ -1,25 +1,45 @@
 require 'json'
 
-######## Instance vars defined
-@jsonData = nil
+######## Important vars defined, variables that shouldn't change much
+jsonData = nil
+@citiesHash = Hash.new
 
 ######## Method definitions
+
 def jsonParseCities
+	parsedData = nil
 	File.open('data/USA_cities.json') do |f|
-		@jsonData = JSON.parse(f.read)
+		parsedData = JSON.parse(f.read)
 		# This block automatically closes File.open
+	end
+	return parsedData
+end
+
+def populateCitiesHash(jsonData)
+	cityDuplicateHash = Hash.new # Use this to track multiple cities
+	
+	jsonData.each do |obj|
+		if obj['longitude'] > -125 # no Alaska or Hawaii please.
+			city = obj['city']
+			if cityDuplicateHash[city] != nil
+				# Increment and append
+				cityDuplicateHash[city] += 1
+			else
+				# First time this city detected
+				cityDuplicateHash[city] = 1
+			end
+			city = city + cityDuplicateHash[city].to_s
+			puts city
+		end
 	end
 end
 
 ######## Script begins
 
-jsonParseCities
+jsonData = jsonParseCities
+
+populateCitiesHash(jsonData)
 
 
-@jsonData.each do |obj|
-	if obj['longitude'] > -125 # no Alaska or Hawaii please.
-		puts obj['city']
-	end
-end
 
 # darn: grep -irn springfield data/USA_cities.json
