6c8b4948492880086ee6dd92735be9a9d66269c2
vagrant <kindzerske@gmail.com>
2019-04-04 10:48:57 +0000

    Data saved in array format: both data and labels

--- main.rb
+++ main.rb
@@ -3,9 +3,10 @@ require 'json'
 ######## Important vars defined, variables that shouldn't change much
 jsonData = nil
 @citiesHash = Hash.new
+@citiesData = []
+@citiesLabels = []
 
 ######## Method definitions
-
 def jsonParseCities
 	parsedData = nil
 	File.open('data/USA_cities.json') do |f|
@@ -15,12 +16,16 @@ def jsonParseCities
 	return parsedData
 end
 
-def populateCitiesHash(jsonData)
+def populateCitiesHashAndDataArray(jsonData)
 	cityDuplicateHash = Hash.new # Use this to track multiple cities
 	
 	jsonData.each do |obj|
 		if obj['longitude'] > -125 # no Alaska or Hawaii please.
+			# 
 			city = obj['city']
+			long = obj['longitude'].to_f
+			lat  = obj['latitude'].to_f
+			
 			if cityDuplicateHash[city] != nil
 				# Increment and append
 				cityDuplicateHash[city] += 1
@@ -28,16 +33,23 @@ def populateCitiesHash(jsonData)
 				# First time this city detected
 				cityDuplicateHash[city] = 1
 			end
-			city = city + cityDuplicateHash[city].to_s
-			@citiesHash[city] = {'longitude': obj['longitude'], 'latitude': obj['latitude'] }
+			city = city + cityDuplicateHash[city].to_s # Appends the number to the city
+			
+			# Add to the hash (to track city uniqueness)
+			@citiesHash[city] = {'longitude': long, 'latitude': lat }
+			
+			# Add to the arrays (which will be fed into the kmeans module)
+			#  (each row corresponding to the other arrays respective row)
+			@citiesLabels.push(city)
+			@citiesData.push( [long, lat] )
 		end
 	end
 end
 
 ######## Script begins
-
 jsonData = jsonParseCities
 
-populateCitiesHash(jsonData)
+populateCitiesHashAndDataArray(jsonData)
 
-puts @citiesHash.inspect
+puts @citiesData[0..3].inspect
+puts @citiesLabels[0..3].inspect
